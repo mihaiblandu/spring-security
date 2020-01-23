baa9af9b3221a29e229f4261c365adc98c81156b
mihaiblandu <mihaiblandu@icloud.com>
2020-01-23 19:28:04 +0200

    fix tests

--- src/test/java/com/example/springsecurity/SpringSecurityApplicationTests.java
+++ src/test/java/com/example/springsecurity/SpringSecurityApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.springsecurity;
 
+import com.example.springsecurity.controller.HomeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringSecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private HomeController controller;
+
+	@Test
+	public void contexLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
