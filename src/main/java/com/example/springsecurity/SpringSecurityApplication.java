1e7b0d2b2ca3dd50473363ba6492c87d66270835
mihaiblandu <mihaiblandu@icloud.com>
2020-01-23 23:22:45 +0200

    ssl setup

--- src/main/java/com/example/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/example/springsecurity/SpringSecurityApplication.java
@@ -1,7 +1,13 @@
 package com.example.springsecurity;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,6 +30,29 @@ public class SpringSecurityApplication {
 						.resourceChain(false);
 			}
 		};
+	}
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {@Override
+		protected void postProcessContext(Context context) {
+			SecurityConstraint securityConstraint = new SecurityConstraint();
+			securityConstraint.setUserConstraint("CONFIDENTIAL");
+			SecurityCollection collection = new SecurityCollection();
+			collection.addPattern("/*");
+			securityConstraint.addCollection(collection);
+			context.addConstraint(securityConstraint);
+		}
+		};
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
+	}
+	private Connector redirectConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8083);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
 	}
 
 }
